c20ae1535ccc64fbada15d17367b7d1b19f3867b
Pmsilva1 <pmsilva1@gmail.com>
2018-10-18 09:40:08 +0100

    fuck you gui

---
+++ src/RMIServer/RMIServer.java
@@ -0,0 +1,37 @@
+import java.net.MulticastSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.io.IOException;
+
+public class RMIServer extends Thread {
+    private String MULTICAST_ADDRESS = "224.3.2.1";
+    private int PORT = 4321;
+    private long SLEEP_TIME = 5000;
+
+    public static void main(String[] args) {
+        RMIServer client = new RMIServer();
+        client.start();
+    }
+
+    public void run() {
+        MulticastSocket socket = null;
+        long counter = 0;
+        try {
+            socket = new MulticastSocket();  // create socket without binding it (only for sending)
+            while (true) {
+                String message = this.getName() + " packet " + counter++;
+                byte[] buffer = message.getBytes();
+
+                InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
+                socket.send(packet);
+
+                try { sleep((long) (Math.random() * SLEEP_TIME)); } catch (InterruptedException e) { }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            socket.close();
+        }
+    }
+}
